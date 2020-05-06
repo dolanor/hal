5e3d17dfd4db4caef5bc99d6971803868e0e3815
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2020-05-06 22:03:49 +0200

    add X timeout check + periodic sending to wakatime

--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/dolanor/hal
 go 1.14
 
 require (
-	github.com/BurntSushi/xgb v0.0.0-20200324125942-20f126ea2843 // indirect
+	github.com/BurntSushi/xgb v0.0.0-20200324125942-20f126ea2843
 	github.com/BurntSushi/xgbutil v0.0.0-20190907113008-ad855c713046
 	github.com/aquilax/go-wakatime v0.1.1
+	github.com/coreos/go-systemd/v22 v22.0.1-0.20200316104309-cb8b64719ae3
+	github.com/godbus/dbus v4.1.0+incompatible // indirect
 )
 
-replace github.com/aquilax/go-wakatime v0.1.1 => /home/dolanor/go/src/github.com/aquilax/go-wakatime
+replace (
+	github.com/aquilax/go-wakatime v0.1.1 => /home/dolanor/go/src/github.com/aquilax/go-wakatime
+	github.com/coreos/go-systemd/v22 v22.0.1-0.20200316104309-cb8b64719ae3 => /home/dolanor/go/src/github.com/coreos/go-systemd
+)
--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"time"
 
+	"github.com/BurntSushi/xgb/screensaver"
+	"github.com/BurntSushi/xgb/xproto"
 	"github.com/BurntSushi/xgbutil"
 	"github.com/BurntSushi/xgbutil/ewmh"
 	"github.com/aquilax/go-wakatime"
+	"github.com/coreos/go-systemd/v22/login1"
 )
 
 func main() {
@@ -16,11 +19,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	currDesk, err := ewmh.CurrentDesktopGet(X)
+	err = screensaver.Init(X.Conn())
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Println("curr desk:", currDesk)
+
+	setup := xproto.Setup(X.Conn())
+	rootWin := setup.DefaultScreen(X.Conn()).Root
+	drw := xproto.Drawable(rootWin)
+	screensaver.SelectInput(X.Conn(), drw, screensaver.EventNotifyMask)
 
 	names, err := ewmh.DesktopNamesGet(X)
 	if err != nil {
@@ -28,7 +35,7 @@ func main() {
 	}
 	fmt.Println("desk names:", names)
 
-	err = ewmh.DesktopNamesSet(X, []string{"desk0", "logcollector", "desk2", "desk3", "desk4", "desk5", "desk6"})
+	err = ewmh.DesktopNamesSet(X, deskNames)
 	if err != nil {
 		panic(err)
 	}
@@ -39,26 +46,79 @@ func main() {
 	}
 	fmt.Println("desk names:", names)
 
-	if int(currDesk) > len(names) {
-		log.Println("name not set for desk:", currDesk)
-		return
-	}
-	projectName := names[currDesk]
-	fmt.Println("curr desk name:", projectName)
-
 	wktr := wakatime.NewBasicTransport(wkKey)
 	wk := wakatime.New(wktr)
 
-	hbResp, err := wk.PostHeartbeat("current", wakatime.HeartbeatItem{
-		Entity:   "gnome workspace",
-		Project:  projectName,
-		Time:     float32(time.Now().UTC().Unix()),
-		Language: "Go",
-	})
+	l, err := login1.New()
 	if err != nil {
-		log.Println(err)
-		return
+		panic(err)
 	}
-	fmt.Println("hbResp:", hbResp)
+	defer l.Close()
+	users, err := l.ListUsers()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("users:", users)
 
+	sessions, err := l.ListSessions()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sessions:", sessions)
+
+	sess, err := l.GetActiveSession()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sess:", sess)
+
+	user, err := l.GetSessionUser(sess)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("user:", user)
+
+	for range time.Tick(time.Second * 5) {
+		info, err := screensaver.QueryInfo(X.Conn(), xproto.Drawable(rootWin)).Reply()
+		if err != nil {
+			log.Fatal(err)
+		}
+		inactiveFor := time.Duration(info.MsSinceUserInput) * time.Millisecond
+		fmt.Println("Inactive for", inactiveFor)
+
+		if inactiveFor > time.Second*30 {
+			println("ignoring this activity")
+			continue
+		}
+
+		sess, err := l.GetActiveSession()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("sess:", sess)
+		continue
+		currDesk, err := ewmh.CurrentDesktopGet(X)
+		if err != nil {
+			panic(err)
+		}
+		if int(currDesk) > len(names) {
+			log.Println("name not set for desk:", currDesk)
+			return
+		}
+
+		projectName := names[currDesk]
+		fmt.Println("curr desk name:", projectName)
+		fmt.Println("curr desk:", currDesk)
+		hbResp, err := wk.PostHeartbeat("current", wakatime.HeartbeatItem{
+			Entity:   "gnome workspace",
+			Project:  projectName,
+			Time:     float32(time.Now().UTC().Unix()),
+			Language: "Go",
+		})
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println("hbResp:", hbResp)
+	}
 }
